7b46cc822b5c31fcf8992dae6c76c8fe2b409d0d
sofa797 <koch.sof.2006@gmail.com>
2025-12-19 02:01:39 +0300

    add k-way merge and tests

--- operations/k-way-merge/KwayMerge.h
+++ operations/k-way-merge/KwayMerge.h
@@ -5,14 +5,65 @@
 #include <optional>
 #include <vector>
 #include <filesystem>
+#include <queue>
 
+
+template<typename T>
+static std::vector<T> merge(const std::vector<std::vector<T>>& blocks){
+    struct Node{
+        T v;
+        size_t blockindx;
+        size_t elindx;
+        bool operator>(const Node& other) const{
+            return v > other.v;
+        }
+    };
+    std::priority_queue<Node, std::vector<Node>, std::greater<Node>> prior_q;
+    std::vector<T> result;
+    for (size_t i=0; i < blocks.size(); ++i){
+        if (!blocks[i].empty()) {
+            prior_q.push(Node{blocks[i][0],i,0});
+        }
+    }
+    while (!prior_q.empty()){
+        Node node = prior_q.top();
+        prior_q.pop();
+        result.push_back(node.v);
+        size_t nextindx = node.elindx+1;
+        if (nextindx < blocks[node.blockindx].size()) {
+            prior_q.push(Node{blocks[node.blockindx][nextindx],node.blockindx,nextindx});
+        }
+    }
+    return result;
+}
 
 template<typename T>
 std::vector<T> KwayMergeSort(const std::vector<T>& array
     , std::size_t k = 4
     , std::size_t block_size = 8)
 {
-    /*Todo*/
-    return std::vector<T>();
+    if (array.empty())
+        return {};
+    if (k == 1) {
+        std::vector<T> result = array;
+        std::sort(result.begin(), result.end());
+        return result;
+    }
+    std::vector<std::vector<T>> blocks;
+    for (size_t start=0; start < array.size(); start+=block_size) {
+        size_t end=std::min(start+block_size, array.size());
+        std::vector<T> block(array.begin()+start, array.begin()+end);
+        std::sort(block.begin(), block.end());
+        blocks.push_back(std::move(block));
+    }
+    while (blocks.size() > 1) {
+        std::vector<std::vector<T>> newblocks;
+        for (size_t i=0; i < blocks.size(); i+=k) {
+            size_t end = std::min(i+k, blocks.size());
+            std::vector<std::vector<T>> domerge(blocks.begin()+i, blocks.begin()+end);
+            newblocks.push_back(merge(domerge));
+        }
+        blocks = std::move(newblocks);
+    }
+    return blocks[0];
 }
-
--- operations/k-way-merge/unit_tests/merge_test.cpp
+++ operations/k-way-merge/unit_tests/merge_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <vector>
 #include "../KwayMerge.h"
+#include <algorithm>
 
 class MergeTest : public ::testing::Test {
 protected:
@@ -11,9 +12,100 @@ protected:
     }
 };
 
-TEST_F(MergeTest, PutAndGet) {
+TEST_F(MergeTest, EmptyArray) {
     std::vector<int> tmp;
     EXPECT_EQ(KwayMergeSort(tmp).size(), 0);
 }
 
-/*TODO*/
+TEST_F(MergeTest, OneEl){
+    std::vector<int> tmp = {0};
+    auto result = KwayMergeSort(tmp);
+    std::vector<int> expected = {0};
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, Repetitions){
+    std::vector<int> tmp = {3, 5, 777, 5, 5, 777, 3, 3, 3, 777, 5};
+    auto result = KwayMergeSort(tmp);
+    std::vector<int> expected = {3, 3, 3, 3, 5, 5, 5, 5, 777, 777, 777};
+    EXPECT_EQ(result, expected);
+}
+
+ TEST_F(MergeTest, OneBlock){
+    std::vector<int> tmp = {4, 78, 1258, 345, 89, 456, 1};
+    auto result = KwayMergeSort(tmp, 1);
+    std::vector<int> expected= {1, 4, 78, 89, 345, 456, 1258};
+    EXPECT_EQ(result, expected);
+ }
+
+TEST_F(MergeTest, KMoreThanNumOfBlocks){
+    std::vector<int> tmp = {5, 32, 8, 10, 1};
+    auto result = KwayMergeSort(tmp, 10, 2);
+    std::vector<int> expected = {1, 5, 8, 10,32};
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, ArrLessThanBlockSize){
+    std::vector<int> tmp = {9, 8, 11, 6};
+    auto result = KwayMergeSort(tmp, 4, 9);
+    std::vector<int> expected = {6, 8, 9, 11};
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, AscendArr_Sorted){
+    std::vector<int> tmp = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto result = KwayMergeSort(tmp);
+    std::vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, DescendArr_ReversedSorted){
+    std::vector<int> tmp = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    auto result = KwayMergeSort(tmp);
+    std::vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, ManyNumbers) {
+    const size_t n = 100000;
+    std::vector<int> tmp(n);
+    for (size_t i=0; i<n; ++i) {
+        tmp[i] = rand()%200000 - 100000;
+    }
+    auto result = KwayMergeSort(tmp);
+    std::vector<int> expected = tmp;
+    std::sort(expected.begin(), expected.end());
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, Strings){
+    std::vector<std::string> tmp = {"aaa", "a", "aaaaaaa", "aa"};
+    auto result = KwayMergeSort(tmp);
+    std::vector<std::string>expected = {"a", "aa", "aaa", "aaaaaaa"};
+    EXPECT_EQ(result, expected);
+}
+TEST_F(MergeTest, DoublesAndNegatives){
+    std::vector<double> tmp = {3.14, -1.0, 2.718, 0.0, -2.5};
+    auto result = KwayMergeSort(tmp);
+    std::vector<double> expected = {-2.5, -1.0, 0.0, 2.718, 3.14};
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, Pairs){
+    std::vector<std::pair<float, char>> tmp = {{3.1, 'b'}, {1.8, 'a'}, {3.1, 'a'}, {2.6, 'z'}};
+    auto result = KwayMergeSort(tmp);
+    std::vector<std::pair<float, char>> expected = {{1.8, 'a'}, {2.6, 'z'}, {3.1, 'a'}, {3.1, 'b'}};
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, Bool){
+    std::vector<bool> tmp = {true, false, true, false, true, false};
+    auto result = KwayMergeSort(tmp, 2, 2);
+    size_t false_count = std::count(result.begin(), result.end(), false);
+    for (size_t i=0; i<false_count; ++i) {
+        EXPECT_FALSE(result[i]);
+    }
+    for (size_t i = false_count; i < result.size(); ++i) {
+        EXPECT_TRUE(result[i]);
+    }
+}
